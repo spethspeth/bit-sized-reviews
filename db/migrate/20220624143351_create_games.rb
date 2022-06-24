19ef48040c70550e93f9e3dc96afebabbd41a29c
Stephen Postma <stephensebastiaanpostma@gmail.com>
2022-06-24 16:58:47 +0200

    basic schema and seed

---
+++ db/migrate/20220624143351_create_games.rb
@@ -0,0 +1,11 @@
+class CreateGames < ActiveRecord::Migration[6.1]
+  def change
+    create_table :games do |t|
+      t.string :name
+      t.integer :year
+      t.string :developer
+
+      t.timestamps
+    end
+  end
+end
